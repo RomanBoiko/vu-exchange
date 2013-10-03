6134923e58966e6d734514de02b0d032849d2421
boikoro <boiko.roman@gmail.com>
2013-10-03 21:46:08 +0100

    basic jeromq receiver for orders

--- src/main/java/vu/exchange/Receiver.java
+++ src/main/java/vu/exchange/Receiver.java
@@ -1,10 +1,19 @@
 package vu.exchange;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.jeromq.ZMQ;
 
 public class Receiver {
+	static class RequestHandler {
+		void onMessage(String message) {
+			Order order = orderFromString(message);
+		}
+	}
+
 	static Order orderFromString(String json) {
 		try {
 			JsonFactory f = new JsonFactory();
@@ -13,6 +22,31 @@ public class Receiver {
 			return mapper.readValue(parser, Order.class);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
+		}
+	}
+
+	static class Server implements Runnable {
+		final RequestHandler handler;
+		volatile AtomicBoolean toStop = new AtomicBoolean(false);
+
+		Server(RequestHandler handler) {
+			this.handler = handler;
+		}
+
+		@Override
+		public void run() {
+			ZMQ.Context context = ZMQ.context(1);
+			ZMQ.Socket socket = context.socket(ZMQ.REP);
+			socket.bind("tcp://127.0.0.1:5555");
+
+			while (!toStop.get()) {
+				byte[] msg = socket.recv(0);
+				handler.onMessage(new String(msg));
+
+				socket.send("{\"received\" : \"ok\"}", 0);
+			}
+			socket.close();
+			context.term();
 		}
 	}
 }
